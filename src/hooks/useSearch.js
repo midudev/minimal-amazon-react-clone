953184bca9aadd92686000641b2e57a32e92f147
midudev <miduga@gmail.com>
2021-08-02 19:57:53 +0200

    Adapt to new object

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -2,13 +2,13 @@ import { useEffect, useState } from 'react'
 import { performSearch as performSearchService } from '../services/performSearch.jsx'
 
 export default function useSearch (params = {}) {
-  const [results, setResults] = useState(null)
+  const [results, setResults] = useState({ hits: null, queryID: null })
 
   const performSearch = (params) => {
     const { allowEmpty, query } = params
 
     if (allowEmpty === false && (query === '' || query === undefined)) {
-      return setResults(null)
+      return setResults({ hits: null, queryID: null })
     }
 
     performSearchService(params).then(setResults)
@@ -20,6 +20,7 @@ export default function useSearch (params = {}) {
 
   return {
     performSearch,
-    results
+    results: results.hits,
+    queryID: results.queryID
   }
 }
