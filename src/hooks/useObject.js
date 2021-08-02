442ef5d2ee2610e2bbb617467afb49dbbefc8d58
midudev <miduga@gmail.com>
2021-08-02 19:55:24 +0200

    Add new hook to retrieve single detail

---
+++ src/hooks/useObject.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+import { getObject as getObjectService } from '../services/performSearch.jsx'
+
+export default function useObject (objectID) {
+  const [object, setObject] = useState(null)
+
+  useEffect(() => {
+    getObjectService(objectID).then(setObject)
+  }, [])
+
+  return {
+    object
+  }
+}
