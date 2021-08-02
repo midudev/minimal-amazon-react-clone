e8221b2bc959d79b4c9bd8ddd47a59bcb4dde08d
midudev <miduga@gmail.com>
2021-08-02 19:55:00 +0200

    Use new properties

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'wouter'
 
-export default function Card ({ objectID, image, title, price }) {
+export default function Card ({ objectID, image_link: image, name, price }) {
   return (
     <div className='Card'>
       <Link href={`/dp/${objectID}`}>
         <a className='Card-anchor'>
-          <img className='Card-image' src={image} alt={title} loading='lazy' />
-          <span className='Card-text'>{title}</span>
+          <img className='Card-image' src={image} alt={name} loading='lazy' />
+          <span className='Card-text'>{name}</span>
           <span className='Card-text'>{price} €</span>
         </a>
       </Link>
